60b64b75cede457b64b0f7d959e534466c8bbe54
Akshay Choukimath <akshay.choukimath@flipkart.com>
2025-01-10 21:44:40 +0530

    add: testing

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
